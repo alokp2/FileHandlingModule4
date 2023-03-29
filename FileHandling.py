a79f08b515cc2b15ce25ebb38d94a32fd587b494
Alok Pandey <62480319+alokp2@users.noreply.github.com>
2023-03-29 11:01:27 +0530

    Add files via upload

---
+++ FileHandling.py
@@ -0,0 +1,24 @@
+'''What is File function in python? What is keywords to create and write
+file.'''
+
+'''File function is used to create,read,and write file using python. Keyword create is used for creating 
+   empty file and write is used for writing to file'''
+
+'''Write a Python program to read an entire text file
+'''
+#open and read file
+f=open("myfile.txt","r")
+print(f.read())
+
+
+#Write a Python program to append text to a file and display the text. 
+#open and append to file
+f= open("myfile.txt","a")
+f.close()
+
+'''Write a Python program to read first n lines of a file. 
+
+'''
+
+
+
